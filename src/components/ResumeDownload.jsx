8e3445fadb0fd6cdd77a045af64b6243177917d4
Ekansh Kumar Chelak <106079488+Ekansh12Chelak@users.noreply.github.com>
2025-07-18 15:52:01 +0530

    Update ResumeDownload.jsx

--- src/components/ResumeDownload.jsx
+++ src/components/ResumeDownload.jsx
@@ -9,7 +9,7 @@ const ResumeDownload = () => {
         Download my resume to explore my skills, experience, and achievements.
       </p>
       <a
-        href="https://drive.google.com/uc?export=download&id=1hi-23fzOuMfEGFZJZIeLh54_BSQIoGoX"
+        href="https://drive.google.com/uc?export=download&id=10qd5W8UWUGHI7NIbK6Ja_qGmYeJLe-rI"
         className="resume-button slideUp"
       >
         <FaDownload className="inline mr-2" /> Download Resume
